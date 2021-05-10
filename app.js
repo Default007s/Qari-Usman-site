ddebef0990b24fd7f7d3e7ace4c1137b0f19d81e
unknown <ahmedjalal0322@gmail.com>
2021-05-10 14:44:35 +0500

    on click effect added

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ document.addEventListener("mousemove", (e) => {
   document.addEventListener("mousemove", (e) => {
     cursor.setAttribute(
       "style",
-      "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px"
+      `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px`
     );
   });
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
 });
